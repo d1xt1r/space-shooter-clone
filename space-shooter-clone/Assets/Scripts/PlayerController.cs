8d265b9071fa2b0b3890e5ebe327eadf71ce98fc
Denislav Georgiev <d1xt1r@gmail.com>
2018-07-27 17:14:42 +0300

    Add PlayerController script. Fix issue in Unity script.

---
+++ space-shooter-clone/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary {
+    public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour {
+
+    Rigidbody playerRigidbody;
+    public float speed;
+    public float tilt;
+    public Boundary boundary;
+    
+
+    void FixedUpdate() {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        playerRigidbody = GetComponent<Rigidbody>();
+
+        playerRigidbody.velocity = movement * speed;
+        playerRigidbody.position = new Vector3 (Mathf.Clamp(playerRigidbody.position.x, boundary.xMin, boundary.xMax), 0.0f, Mathf.Clamp(playerRigidbody.position.z, boundary.zMin, boundary.zMax));
+        playerRigidbody.rotation = Quaternion.Euler(0.0f, 0.0f, playerRigidbody.velocity.x * -tilt);
+    }
+}
