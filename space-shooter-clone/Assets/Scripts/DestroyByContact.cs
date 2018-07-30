63d898967cb0ed3a03436ac1dbaf3b9274950e43
Denislav Georgiev <d1xt1r@gmail.com>
2018-07-30 11:47:06 +0300

    Add code to handle explosion if the asteroid hits the player ship

--- space-shooter-clone/Assets/Scripts/DestroyByContact.cs
+++ space-shooter-clone/Assets/Scripts/DestroyByContact.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class DestroyByContact : MonoBehaviour {
 
+    public GameObject asteroidExplosion;
+    public GameObject playerExplosion;
+
     void OnTriggerEnter(Collider other) {
         if (other.tag == "Boundary") {
             return;
+        }
+        Instantiate(asteroidExplosion, transform.position, transform.rotation);
+        if(other.tag == "Player") {
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
         }
         Destroy(other.gameObject); // destorys the laser bolt
         Destroy(gameObject); // destorys the asteriod
